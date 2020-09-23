acbc1783f0e4e705d8299c126e9bf2515b1b3e12
Roman-00 <rvasin00@gmail.com>
2020-09-23 13:23:42 +0600

    add a new animation

--- js/main.js
+++ js/main.js
@@ -23,5 +23,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
     animImg();
 
+});
 
+$(document).ready(function() {
+    $(function() {  
+        $('.btn-6')
+          .on('mouseenter', function(e) {
+                  var parentOffset = $(this).offset(),
+                    relX = e.pageX - parentOffset.left,
+                    relY = e.pageY - parentOffset.top;
+                  $(this).find('span').css({top:relY, left:relX})
+          })
+          .on('mouseout', function(e) {
+                  var parentOffset = $(this).offset(),
+                    relX = e.pageX - parentOffset.left,
+                    relY = e.pageY - parentOffset.top;
+              $(this).find('span').css({top:relY, left:relX})
+          });
+        $('[href=#]').click(function(){return false});
+      });
 });
